f20275427affce620740d5146e14aa22036aba29
Ryan Pirotta <rpirotta@bluefort.eu>
2025-05-03 12:30:47 +0200

    Updating main class, reducing code duplication

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import backtrader as bt
 import pandas as pd
 import config
+import matplotlib.pyplot as plt
+
 from backtests.SuperTrend.supertrend import SuperTrend, PandasDataFeed
 
 
 if __name__ == '__main__':
-    timeframe = '4h'
+    timeframe = '1D'
+    date_format = '%d/%m/%Y %H:%M' if timeframe != '1d' else '%d/%m/%Y'
+
+    # Obtaining values from config file
+    config_filepath_df      = f'btc_{timeframe}_supertrend'
+    config_filepath_returns = f'SuperTrend_vs_BTC_Benchmark_{timeframe}_returns'
+    config_filepath_charts  = f'SuperTrend_Visuals_{timeframe}'
+
+    data_path           = getattr(config, config_filepath_df)
+    data_path_returns   = getattr(config, config_filepath_returns)
+    data_path_charts    = getattr(config, config_filepath_charts)
 
     cerebro = bt.Cerebro()
 
@@ -16,34 +28,12 @@ if __name__ == '__main__':
     # Position size of 95%
     cerebro.addsizer(bt.sizers.PercentSizer, percents=95)
 
-    if timeframe == "15m":
-        dataframe = pd.read_csv(
-            config.btc_15m_supertrend,
-            usecols=['datetime', 'open', 'high', 'low', 'close', 'volume', 'in_uptrend'],
-            index_col='datetime',
-            parse_dates=['datetime'],
-            date_format='%d/%m/%Y %H:%M')
-    elif timeframe == '1h':
-        dataframe = pd.read_csv(
-            config.btc_1h_supertrend,
-            usecols=['datetime', 'open', 'high', 'low', 'close', 'volume', 'in_uptrend'],
-            index_col='datetime',
-            parse_dates=['datetime'],
-            date_format='%d/%m/%Y %H:%M')
-    elif timeframe == '4h':
-        dataframe = pd.read_csv(
-            config.btc_4h_supertrend,
-            usecols=['datetime', 'open', 'high', 'low', 'close', 'volume', 'in_uptrend'],
-            index_col='datetime',
-            parse_dates=['datetime'],
-            date_format='%d/%m/%Y %H:%M')
-    elif timeframe == '1d':
-        dataframe = pd.read_csv(
-            config.btc_1d_supertrend,
-            usecols=['datetime', 'open', 'high', 'low', 'close', 'volume', 'in_uptrend'],
-            index_col='datetime',
-            parse_dates=['datetime'],
-            date_format='%d/%m/%Y')
+    dataframe = pd.read_csv(
+        data_path,
+        usecols=['datetime', 'open', 'high', 'low', 'close', 'volume', 'in_uptrend'],
+        index_col='datetime',
+        parse_dates=['datetime'],
+        date_format=date_format)
 
     # Converting the datetime field to datetime
     dataframe.index = pd.to_datetime(dataframe.index)
@@ -60,9 +50,42 @@ if __name__ == '__main__':
     cerebro.addstrategy(SuperTrend)
 
     # Run backtrader
-    cerebro.run()
+    strats = cerebro.run()
+    strategy_instance = strats[0]
+
+    # Returns from the SuperTrend Strategy on Historical data
+    returns = pd.Series(strategy_instance.portfolio_values, index=strategy_instance.dates)
+
+    # Returns from the BTC Benchmark on Historical data 2018-01-01 to 2025-04-30
+    btc_returns = dataframe['close'].pct_change().dropna()
+
+    # Convert equity to returns
+    strat_returns = returns.pct_change().dropna()
+
+    # Combine both return series into a single DataFrame
+    combined_returns = pd.DataFrame({
+        'superTrend_return': strat_returns,
+        'btc_return': btc_returns
+    }, index=returns.index)
+
+
+    combined_returns.dropna(inplace=True)
+
+    # Reset index to move datetime from index to a column
+    combined_returns = combined_returns.reset_index()
+
+    # Rename the index column to something clear (e.g., 'datetime')
+    combined_returns.rename(columns={'index': 'datetime'}, inplace=True)
+
+    # Saving SuperTrend and BTC Benchmark returns
+    combined_returns.to_csv(data_path_returns)
 
     # Plot Chart with the positions executed and portfolio visualizations
-    cerebro.plot()
+    figs = cerebro.plot()
+
+    fig = figs[0][0]
+
+    # Saving Chart for Visualization
+    fig.savefig(data_path_charts)
 
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
